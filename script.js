ffb7834b7e90898118d9512505ddacca69543388
Cosmic0921 <85378869+Cosmic0921@users.noreply.github.com>
2026-02-05 14:21:40 -0600

    Change some logic

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const wordHistory = document.querySelector(".previousWords");
 const guessHTML = document.querySelector(".status #gusses");
 const minesHTML = document.querySelector(".status #mines");
 
-let guessCounter = 20;
+let guessCounter = 25;
 let minesRemain = 5;
 
 guessHTML.innerText = `${guessCounter} Guesses`;
@@ -11,11 +11,10 @@ minesHTML.innerText = `${minesRemain} 💣`;
 
 enterButton.addEventListener("click", function () {
     if (guessCounter > 0) {
-        guessCounter--;
         guessHTML.innerText = `${guessCounter} Guesses`;
-
         let testText = document.createElement("p");
-        testText.innerText = `Word ${21-guessCounter}`;
+        testText.innerText = `Word ${26-guessCounter}`;
         wordHistory.append(testText);
+        guessCounter--;
     }
 });
